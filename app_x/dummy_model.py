c7bc1a596d43360c057572aa2237cdd70c347ac2
Nhomar Hernández [Vauxoo] <nhomar@vauxoo.com>
2016-05-16 23:05:54 -0500

    This test does not belong to this repository. (#1)

--- app_x/dummy_model.py
+++ app_x/dummy_model.py
@@ -10,9 +10,3 @@ class DummyModel(orm.Model):
     _columns = {
         'name': fields.char('Dummy', size=100),
     }
-
-
-# printout non-ASCII text to check unicode issues
-# (from Monty Python's "Italian Lesson"...)
-sys.stdout.write("Eeeeeee! Milano è tanto meglio di Napoli. "
-                 "Milano è la citta la più bella di tutti ... nel mondo...\n")
